0cfb043749da95a16e25b1d4d9d3508ee094d6b4
Tanawat Tassana <amp.tanawat@gmail.com>
2012-12-30 12:35:37 +0700

    init

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Prapin Village</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class="container">
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<img class="brand-logo" src="images/logo-pp.png" alt="">
+				<a class="brand" href="#">Prapin Village Phuket</a>
+			</div>
+		</div>
+		<div class="header">
+			<div>
+				<img src="images/logo-220.png" alt="">
+			</div>
+		</div>
+		<div class="hl row">
+			<div class="hl-item span3">
+				<img src="images/single.jpg" alt="" class="img-polaroid">
+				<span class="hl-item-label">บ้านเดี่ยวชั้นเดียว</span>
+			</div>
+			<div class="hl-item span3 hl-item-down">
+				<img src="images/townhouse-2.jpg" alt="" class="img-polaroid">
+				<span class="hl-item-label">ทาวน์เฮาส์สองชั้น</span>
+			</div>
+			<div class="hl-item span3 hl-item-down">
+				<img src="images/townhome-3.jpg" alt="" class="img-polaroid">
+				<span class="hl-item-label">ทาวน์โฮมสามชั้น</span>
+			</div>
+			<div class="hl-item span3">
+				<img src="images/twinhome-2.jpg" alt="" class="img-polaroid">
+				<span class="hl-item-label">บ้านแฝดสองชั้น</span>
+			</div>
+		</div>
+		<div class="project-title">
+			<h3><img src="images/logo-pp.png" alt="" style="width:32px;">&nbsp;โครงการหมู่บ้านพระปิ่น ภูเก็ต</h3>
+		</div>
+		<div class="row">
+			<div class="map-container span">
+				<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.th/maps?q=7.845654,98.339169&amp;num=1&amp;t=m&amp;ie=UTF8&amp;ll=7.845016,98.337722&amp;spn=0.028952,0.043516&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.co.th/maps?q=7.845654,98.339169&amp;num=1&amp;t=m&amp;ie=UTF8&amp;ll=7.845016,98.337722&amp;spn=0.028952,0.043516&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+			</div>
+			<div class="span6">
+				<h4>สำนักงานขาย(ชั่วคราว)</h4>
+				<address>
+					ถนนหลวงพ่อแช่ม (ซอยด้านหน้าวัดฉลอง) หมู่ที่ 4 ตำบล ฉลอง อำเภอ เมือง จังหวัด ภูเก็ต<br/>
+					<abbr title="phone">ติดต่อ</abbr> 081-895-5516
+				</address>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
